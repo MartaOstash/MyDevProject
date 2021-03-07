a7698d7b0f69c8c2836a091c2089c3dd2cbec403
MartaOstash <martaostash.18@gmail.com>
2021-03-07 11:23:03 -0500

    Testing pipeline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 	// write your code here
-        for (int i = 0; i < 1; i++) {
-            stem.out.println("Hello Batch 8");
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Hello Batch 8");
             Thread.sleep(2000);
 
         }
