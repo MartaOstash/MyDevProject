f149ce3b8c6e77208c02ceeed44e673724c6ad93
MartaOstash <martaostash.18@gmail.com>
2021-03-07 10:55:11 -0500

    BUild should fail

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 	// write your code here
         for (int i = 0; i < 1; i++) {
-            System.out.println("Hello Batch 8");
+            ystem.out.println("Hello Batch 8");
             Thread.sleep(2000);
 
         }
